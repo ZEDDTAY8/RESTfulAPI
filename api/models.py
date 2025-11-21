93a12519cd4f01cbe22f5ef791caeb34f735326f
ZEDDTAY8 <zeddtay123@gmail.com>
2025-11-21 19:25:41 +0500

    Добавление моделей: Author, Genre, Book

---
+++ api/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
